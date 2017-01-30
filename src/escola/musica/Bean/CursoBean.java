23661fcd7aff14db368b5f9a32fbc6c204b26f4c
JhonDonavan <jhondonavan@gmail.com>
2017-01-30 19:31:47 -0200

    Form ok

--- src/escola/musica/Bean/CursoBean.java
+++ src/escola/musica/Bean/CursoBean.java
@@ -3,7 +3,9 @@ package escola.musica.Bean;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import escola.musica.modelo.Curso;
 import escola.musica.modelo.TipoCurso;
@@ -13,6 +15,10 @@ public class CursoBean {
 
 	private Curso curso = new Curso();
 	private List<TipoCurso> tipos = Arrays.asList(TipoCurso.values());
+	
+	public void salvar(){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Curso salvo com sucesso"));
+	}
 
 	public Curso getCurso() {
 		return curso;
