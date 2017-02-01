5df496ffe0d34e4d9dd8154ccb2e2acaef6f7e8f
JhonDonavan <jhondonavan@gmail.com>
2017-02-01 11:08:07 -0200

    Metodo listarTodos no CursoDAO

--- src/escola/musica/dao/CursoDAO.java
+++ src/escola/musica/dao/CursoDAO.java
@@ -1,20 +1,32 @@
 package escola.musica.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import escola.musica.modelo.Curso;
 
 public class CursoDAO {
-	
-		public void salvar(Curso curso){
-			EntityManager entityManager = JPAUtil.getEntityManager();
-			
-			entityManager.getTransaction().begin();
-			
-			entityManager.merge(curso);
-			
-			entityManager.getTransaction().commit();
-			
-			entityManager.close();
-		}
+
+	public void salvar(Curso curso) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		entityManager.getTransaction().begin();
+
+		entityManager.merge(curso);
+
+		entityManager.getTransaction().commit();
+
+		entityManager.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Curso> listarTodos() {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		Query query = entityManager.createQuery("from Curso");
+		
+		return query.getResultList();
+	}
 }
